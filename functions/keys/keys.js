e712f5370d96288489ce470a420bf7fa2ed3129a
AlessandroCofone <dantecofone@gmail.com>
2023-12-04 17:38:10 +0100

    new keys

--- functions/keys/keys.js
+++ functions/keys/keys.js
@@ -1,27 +1,12 @@
-//.netlify/functions/keys.js
-exports.handler = async function (event, context) {
-  try {
-    // Fetch and return the API keys from your secure storage
-    const googleApiKey = process.env.GOOGLE_API_KEY;
-    const astrologyUserId = process.env.ASTROLOGY_USER_ID;
-    const astrologyApiKey = process.env.ASTROLOGY_API_KEY;
+// netlify/functions/keys.js
 
-    return {
-      statusCode: 200,
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Headers': 'Content-Type',
-      },
-      body: JSON.stringify({
-        googleApiKey,
-        astrologyUserId,
-        astrologyApiKey,
-      }),
-    };
-  } catch (error) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: 'Internal Server Error' }),
-    };
-  }
+exports.handler = async function (event, context) {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      googleApiKey: process.env.GOOGLE_API_KEY,
+      astrologyUserId: process.env.ASTROLOGY_USER_ID,
+      astrologyApiKey: process.env.ASTROLOGY_API_KEY,
+    }),
+  };
 };
