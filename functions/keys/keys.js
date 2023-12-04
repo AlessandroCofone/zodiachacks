1c28ea5a81d32a4af8be7af5e012ae8a7e969ed4
AlessandroCofone <dantecofone@gmail.com>
2023-12-04 16:22:45 +0100

    fix cors 2

--- functions/keys/keys.js
+++ functions/keys/keys.js
@@ -3,6 +3,10 @@
 exports.handler = async function (event, context) {
   return {
     statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      // Add other necessary headers here
+    },
     body: JSON.stringify({
       googleApiKey: process.env.GOOGLE_API_KEY,
       astrologyUserId: process.env.ASTROLOGY_USER_ID,
