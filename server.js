1b04f698116a742ea82d8bbd59bd61dc3a402e5f
AlessandroCofone <dantecofone@gmail.com>
2023-12-04 15:47:37 +0100

    backend

--- server.js
+++ server.js
@@ -1,28 +1,69 @@
 const express = require('express');
+const axios = require('axios');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
 const app = express();
 const port = process.env.PORT || 3000;
 
-// server.js
-exports.handler = async function (event, context) {
-    return {
-      statusCode: 200,
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        // Add other necessary headers
-      },
-      body: JSON.stringify({ message: 'Hello World' }),
-    };
-  };  
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-  // Expose credentials from .env file
-  res.json({
-    GOOGLE_API_KEY: process.env.GOOGLE_API_KEY,
-    ASTROLOGY_USER_ID: process.env.ASTROLOGY_USER_ID,
-    ASTROLOGY_API_KEY: process.env.ASTROLOGY_API_KEY,
-  });
+app.get('/api/keys', (req, res) => {
+  const keys = {
+    googleApiKey: process.env.GOOGLE_API_KEY,
+    astrologyUserId: process.env.ASTROLOGY_USER_ID,
+    astrologyApiKey: process.env.ASTROLOGY_API_KEY,
+  };
+
+  res.json(keys);
 });
 
+app.post('/api/location', async (req, res) => {
+  const { location } = req.body;
+
+  try {
+    const geoData = await axios.get('https://maps.googleapis.com/maps/api/geocode/json', {
+      params: {
+        address: location,
+        key: process.env.GOOGLE_API_KEY,
+      },
+    });
+
+    if (geoData.data.results && geoData.data.results.length > 0) {
+      const locationData = geoData.data.results[0].geometry.location;
+      const timezone = await getTimeZone(locationData.lat, locationData.lng);
+
+      res.json({ locationData, timezone });
+    } else {
+      res.status(404).json({ error: 'Location not found. Please enter a valid location.' });
+    }
+  } catch (error) {
+    console.error('Google Maps API request failed:', error.message);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+function getTimeZone(lat, lon) {
+  return axios.get('https://maps.googleapis.com/maps/api/timezone/json', {
+    params: {
+      location: `${lat},${lon}`,
+      timestamp: Math.floor(Date.now() / 1000),
+      key: process.env.GOOGLE_API_KEY,
+    },
+  })
+  .then(response => {
+    const timezone = ((response.data.rawOffset / 3600) + (response.data.dstOffset / 3600)).toFixed(1);
+    return timezone;
+  })
+  .catch(error => {
+    console.error('Google Maps Time Zone API request failed:', error.message);
+    return '0.0';
+  });
+}
+
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+  console.log(`Server is running on port ${port}`);
 });
