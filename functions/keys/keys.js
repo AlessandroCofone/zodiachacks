426f2c177568f8c0939df833fd6386a35c81df9c
AlessandroCofone <dantecofone@gmail.com>
2023-12-04 17:58:06 +0100

    keys.js edit

--- functions/keys/keys.js
+++ functions/keys/keys.js
@@ -1,12 +1,22 @@
-// netlify/functions/keys.js
-
 exports.handler = async function (event, context) {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
+  try {
+    // Your existing logic to fetch API keys
+    const keys = {
       googleApiKey: process.env.GOOGLE_API_KEY,
       astrologyUserId: process.env.ASTROLOGY_USER_ID,
       astrologyApiKey: process.env.ASTROLOGY_API_KEY,
-    }),
-  };
+    };
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(keys),
+    };
+  } catch (error) {
+    console.error('Error fetching API keys:', error);
+
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Internal Server Error' }),
+    };
+  }
 };
