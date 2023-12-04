c3a57a796a25bafa6a646d7fb3cc287766bfa816
AlessandroCofone <dantecofone@gmail.com>
2023-12-04 12:06:06 +0100

    8

--- server.js
+++ server.js
@@ -2,15 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  // Expose credentials from .env file
-  res.json({
-    GOOGLE_API_KEY: process.env.GOOGLE_API_KEY,
-    ASTROLOGY_USER_ID: process.env.ASTROLOGY_USER_ID,
-    ASTROLOGY_API_KEY: process.env.ASTROLOGY_API_KEY,
-  });
-});
-
+// server.js
 exports.handler = async function (event, context) {
     return {
       statusCode: 200,
@@ -20,7 +12,16 @@ exports.handler = async function (event, context) {
       },
       body: JSON.stringify({ message: 'Hello World' }),
     };
-  };
+  };  
+
+app.get('/', (req, res) => {
+  // Expose credentials from .env file
+  res.json({
+    GOOGLE_API_KEY: process.env.GOOGLE_API_KEY,
+    ASTROLOGY_USER_ID: process.env.ASTROLOGY_USER_ID,
+    ASTROLOGY_API_KEY: process.env.ASTROLOGY_API_KEY,
+  });
+});
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
